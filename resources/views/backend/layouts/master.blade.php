88f91f2837e3536872fe047aeb7915af7ec3d3ae
Thanh <mit26101998@gmail.com>
2020-12-03 20:55:19 +0700

    loi

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -9,34 +9,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/fontawesome-free/css/all.min.css')}}">
 
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
 
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
 
   <!-- JQVMap -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/jqvmap/jqvmap.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/jqvmap/jqvmap.min.css')}}">
 
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('/backend/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/dist/css/adminlte.min.css')}}">
 
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
 
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/daterangepicker/daterangepicker.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/daterangepicker/daterangepicker.css')}}">
 
   <!-- summernote -->
-  <link rel="stylesheet" href="{{asset('/backend/plugins/summernote/summernote-bs4.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/plugins/summernote/summernote-bs4.css')}}">
 
   <!-- main -->
-  <link rel="stylesheet" href="{{asset('/backend/dist/css/main.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('/backend/dist/css/main.css')}}">
 
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
@@ -87,51 +87,51 @@
   <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{asset('/backend/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/jquery/jquery.min.js')}}"></script>
 
 <!-- jQuery UI 1.11.4 -->
-<script src="{{asset('/backend/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 
 
 <!-- Bootstrap 4 -->
-<script src="{{asset('/backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- ChartJS -->
-<script src="{{asset('/backend/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/chart.js/Chart.min.js')}}"></script>
 
 <!-- Sparkline -->  
-<script src="{{asset('/backend/plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/sparklines/sparkline.js')}}"></script>
 
 <!-- JQVMap -->
-<script src="{{asset('/backend/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-<script src="{{asset('/backend/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
 
 <!-- jQuery Knob Chart -->
-<script src="{{asset('/backend/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 
 <!-- daterangepicker -->
-<script src="{{asset('/backend/plugins/moment/moment.min.js')}}"></script>
-<script src="{{asset('/backend/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/moment/moment.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/daterangepicker/daterangepicker.js')}}"></script>
 
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{asset('/backend/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 
 <!-- Summernote -->
-<script src="{{asset('/backend/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/summernote/summernote-bs4.min.js')}}"></script>
 
 <!-- overlayScrollbars -->
-<script src="{{asset('/backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{secure_asset('/backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 
 <!-- AdminLTE App -->
-<script src="{{asset('/backend/dist/js/adminlte.js')}}"></script>
+<script src="{{secure_asset('/backend/dist/js/adminlte.js')}}"></script>
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('/backend/dist/js/pages/dashboard.js')}}"></script>
+<script src="{{secure_asset('/backend/dist/js/pages/dashboard.js')}}"></script>
 
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('/backend/dist/js/demo.js')}}"></script>
+<script src="{{secure_asset('/backend/dist/js/demo.js')}}"></script>
 <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 
